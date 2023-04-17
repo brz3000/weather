789cff727f18d62069fceadff263235768d6caa1
brz3000 <45814758+brz3000@users.noreply.github.com>
2023-04-18 02:35:32 +0300

    add funtion main

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import requests
 cities = ["лондон", "шереметьево", "череповец"]
 url_template = 'https://wttr.in/{}?nTqM&lang=ru'
-for city in cities:
-	url = url_template.format(city)
-	response = requests.get(url)
-	print (response.text)
+
+def main():
+	for city in cities:
+		url = url_template.format(city)
+		response = requests.get(url)
+		print (response.text)
+
+if __name__ == '__main__':
+	main()
+	
