77d6705c46b0aac2ed9c7dad0f9ee02bdc526feb
brz3000 <45814758+brz3000@users.noreply.github.com>
2023-04-18 03:07:58 +0300

    formatting for PEP8

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import requests
+
+
 cities = ["лондон", "шереметьево", "череповец"]
 url_template = 'https://wttr.in/{}?nTqM&lang=ru'
 
+
 def main():
-	for city in cities:
-		url = url_template.format(city)
-		response = requests.get(url)
-		print (response.text)
+    for city in cities:
+        url = url_template.format(city)
+        response = requests.get(url)
+        print(response.text)
+
 
 if __name__ == '__main__':
-	main()
-	
+    main()
